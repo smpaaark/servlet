20337b4f3fd14000457f1231d8e8943efdef23e2
smpark1020 <smpark911020@gmail.com>
2021-10-10 22:05:20 +0900

    스프링 MVC - 컨트롤러 통합

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,4 +1,4 @@
-package hello.servlet.web.springmvc.v1;
+package hello.servlet.web.springmvc.v2;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
@@ -8,14 +8,21 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
+@RequestMapping("/springmvc/v2/members")
 @Controller
-public class SpringMemberSaveControllerV1 {
+public class SpringMemberControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/springmvc/v1/members/save")
-    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
@@ -24,6 +31,14 @@ public class SpringMemberSaveControllerV1 {
 
         ModelAndView mv = new ModelAndView("save-result");
         mv.addObject("member", member);
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
         return mv;
     }
 }
